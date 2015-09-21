8376cc6a05c2c7cd0e31d6d7aac6d4e9546434d8
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-09-21 23:27:38 +0300

    First work version

---
+++ README.md
@@ -0,0 +1,16 @@
+shell2telegram
+==============
+
+Create Telegram bot from command-line
+
+Usage:
+
+Get token from [BotFather bot](https://telegram.me/BotFather), and set TB_TOKEN var in shell
+
+    export TB_TOKEN=********
+    shell2telegram /cmd 'shell command' /cmd2 'shell command2'
+
+Links
+-----
+
+  * [Golang bindings for the Telegram Bot API](https://github.com/Syfaro/telegram-bot-api)
---
+++ shell2telegram.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+
+	"github.com/Syfaro/telegram-bot-api"
+)
+
+// version
+const VERSION = "1.0"
+
+func main() {
+	flag.Usage = func() {
+		fmt.Printf("usage: %s [options] /path \"shell command\" /path2 \"shell command2\"\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+	version := flag.Bool("version", false, "get version")
+	flag.Parse()
+	if *version {
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
+
+	// need >= 2 arguments and count of it must be even
+	args := flag.Args()
+	if len(args) < 2 || len(args)%2 == 1 {
+		log.Panic(fmt.Errorf("error: need pairs of path and shell command"))
+	}
+
+	cmd_handlers := map[string]string{}
+
+	for i := 0; i < len(args); i += 2 {
+		path, cmd := args[i], args[i+1]
+		if path[0] != '/' {
+			log.Panic(fmt.Errorf("error: path %s dont starts with /", path))
+		}
+		cmd_handlers[path] = cmd
+	}
+
+	tb_token := os.Getenv("TB_TOKEN")
+	if tb_token == "" {
+		log.Panic("TB_TOKEN env var not found")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(tb_token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
+	ucfg.Timeout = 60
+	err = bot.UpdatesChan(ucfg)
+
+	for {
+		select {
+		case update := <-bot.Updates:
+
+			chat_id := update.Message.Chat.ID
+
+			parts := regexp.MustCompile(`\s+`).Split(update.Message.Text, 2)
+			if len(parts) > 0 {
+				if parts[0] == "/help" {
+					reply := ""
+					for cmd, shell_cmd := range cmd_handlers {
+						reply += fmt.Sprintf("%s\n %s\n", cmd, shell_cmd)
+					}
+					msg := tgbotapi.NewMessage(chat_id, reply)
+					bot.SendMessage(msg)
+				}
+
+				if cmd, found := cmd_handlers[parts[0]]; found {
+					shell, params := "sh", []string{"-c", cmd}
+					if len(parts) > 1 {
+						params = append(params, parts[1])
+					}
+
+					os_exec_command := exec.Command(shell, params...)
+					os_exec_command.Stderr = os.Stderr
+					shell_out, err := os_exec_command.Output()
+					if err != nil {
+						log.Println("exec error: ", err)
+						reply := fmt.Sprintf("exec error: %s", err)
+						msg := tgbotapi.NewMessage(chat_id, reply)
+						bot.SendMessage(msg)
+					} else {
+						reply := string(shell_out)
+						msg := tgbotapi.NewMessage(chat_id, reply)
+						bot.SendMessage(msg)
+					}
+				}
+			}
+		}
+	}
+}
