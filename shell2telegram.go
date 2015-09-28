403097540999d059bce9416e94e7ef6d2e15ad63
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-09-28 20:53:03 +0300

    Renamed vars; refactoring

--- shell2telegram.go
+++ shell2telegram.go
@@ -36,12 +36,12 @@ type Config struct {
 
 // ----------------------------------------------------------------------------
 // get config
-func getConfig() (commands Commands, app_config Config, err error) {
-	flag.StringVar(&app_config.token, "tb-token", "", "setting bot token (or set TB_TOKEN variable)")
-	flag.BoolVar(&app_config.addExit, "add-exit", false, "adding \"/shell2telegram exit\" command for terminate bot (for roots only)")
-	flag.IntVar(&app_config.botTimeout, "timeout", DEFAULT_BOT_TIMEOUT, "setting timeout for bot")
-	flag.BoolVar(&app_config.allowAll, "allow-all", false, "allow all users (DANGEROUS!)")
-	flag.BoolVar(&app_config.logCommands, "log-commands", false, "logging all commands")
+func getConfig() (commands Commands, appConfig Config, err error) {
+	flag.StringVar(&appConfig.token, "tb-token", "", "setting bot token (or set TB_TOKEN variable)")
+	flag.BoolVar(&appConfig.addExit, "add-exit", false, "adding \"/shell2telegram exit\" command for terminate bot (for roots only)")
+	flag.IntVar(&appConfig.botTimeout, "timeout", DEFAULT_BOT_TIMEOUT, "setting timeout for bot")
+	flag.BoolVar(&appConfig.allowAll, "allow-all", false, "allow all users (DANGEROUS!)")
+	flag.BoolVar(&appConfig.logCommands, "log-commands", false, "logging all commands")
 	logFilename := flag.String("log", "", "log filename, default - STDOUT")
 	allowUsers := flag.String("allow-users", "", "telegram users who are allowed to chat with the bot (\"user1,user2\")")
 	rootUsers := flag.String("root-users", "", "telegram users, who confirms new users in their private chat (\"user1,user2\")")
@@ -69,39 +69,39 @@ func getConfig() (commands Commands, app_config Config, err error) {
 
 	// setup users and roots
 	if *allowUsers != "" {
-		app_config.allowUsers = strings.Split(*allowUsers, ",")
+		appConfig.allowUsers = strings.Split(*allowUsers, ",")
 	}
 	if *rootUsers != "" {
-		app_config.rootUsers = strings.Split(*rootUsers, ",")
+		appConfig.rootUsers = strings.Split(*rootUsers, ",")
 	}
 
 	commands = Commands{}
 	// need >= 2 arguments and count of it must be even
 	args := flag.Args()
 	if len(args) < 2 || len(args)%2 == 1 {
-		return commands, app_config, fmt.Errorf("error: need pairs of chat-command and shell-command")
+		return commands, appConfig, fmt.Errorf("error: need pairs of chat-command and shell-command")
 	}
 
 	for i := 0; i < len(args); i += 2 {
 		path, cmd := args[i], args[i+1]
 		if path[0] != '/' {
-			return commands, app_config, fmt.Errorf("error: path %s dont starts with /", path)
+			return commands, appConfig, fmt.Errorf("error: path %s dont starts with /", path)
 		}
 		commands[path] = cmd
 	}
 
-	if app_config.token == "" {
-		if app_config.token = os.Getenv("TB_TOKEN"); app_config.token == "" {
-			return commands, app_config, fmt.Errorf("TB_TOKEN environment var not found. See https://core.telegram.org/bots#botfather for more information\n")
+	if appConfig.token == "" {
+		if appConfig.token = os.Getenv("TB_TOKEN"); appConfig.token == "" {
+			return commands, appConfig, fmt.Errorf("TB_TOKEN environment var not found. See https://core.telegram.org/bots#botfather for more information\n")
 		}
 	}
 
-	return commands, app_config, nil
+	return commands, appConfig, nil
 }
 
 // ----------------------------------------------------------------------------
-func sendMessageWithLogging(bot *tgbotapi.BotAPI, chat_id int, replay_msg string) {
-	_, err := bot.SendMessage(tgbotapi.NewMessage(chat_id, replay_msg))
+func sendMessageWithLogging(bot *tgbotapi.BotAPI, chatId int, replayMsg string) {
+	_, err := bot.SendMessage(tgbotapi.NewMessage(chatId, replayMsg))
 	if err != nil {
 		log.Print("Bot send message error: ", err)
 	}
@@ -109,106 +109,106 @@ func sendMessageWithLogging(bot *tgbotapi.BotAPI, chat_id int, replay_msg string
 
 // ----------------------------------------------------------------------------
 func main() {
-	commands, app_config, err := getConfig()
+	commands, appConfig, err := getConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bot, err := tgbotapi.NewBotAPI(app_config.token)
+	bot, err := tgbotapi.NewBotAPI(appConfig.token)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Printf("Authorized on bot account: %s", bot.Self.UserName)
 
-	var tgbot_config tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
-	tgbot_config.Timeout = app_config.botTimeout
-	err = bot.UpdatesChan(tgbot_config)
+	var tgbotConfig tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
+	tgbotConfig.Timeout = appConfig.botTimeout
+	err = bot.UpdatesChan(tgbotConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	go_exit := false
-	users := NewUsers(app_config)
+	doExit := false
+	users := NewUsers(appConfig)
 	vacuumTicker := time.Tick(SECONDS_FOR_OLD_USERS_BEFORE_VACUUM * time.Second)
 
 LOOP:
 	for {
 		select {
-		case telegram_update := <-bot.Updates:
+		case telegramUpdate := <-bot.Updates:
 
-			chat_id := telegram_update.Message.Chat.ID
-
-			messageText := regexp.MustCompile(`\s+`).Split(telegram_update.Message.Text, 2)
+			messageText := regexp.MustCompile(`\s+`).Split(telegramUpdate.Message.Text, 2)
 			messageCmd, messageArgs := messageText[0], ""
 			if len(messageText) > 1 {
 				messageArgs = messageText[1]
 			}
 
-			replay_msg := ""
+			replayMsg := ""
 
 			if len(messageText) > 0 && len(messageCmd) > 0 && messageCmd[0] == '/' {
 
-				user_from := telegram_update.Message.From
+				userFrom := telegramUpdate.Message.From
 
-				users.AddNew(telegram_update.Message)
-				allowExec := app_config.allowAll || users.IsAuthorized(user_from.ID)
+				users.AddNew(telegramUpdate.Message)
+				allowExec := appConfig.allowAll || users.IsAuthorized(userFrom.ID)
 
-				if messageCmd == "/auth" || messageCmd == "/authroot" {
+				// commands ---
+				switch {
+				case messageCmd == "/auth" || messageCmd == "/authroot":
 
-					for_root := messageCmd == "/authroot"
+					forRoot := messageCmd == "/authroot"
 
 					if len(messageText) == 1 {
 
-						replay_msg = "See code in terminal with shell2telegram or ask code from root user and type:\n" + messageCmd + " code"
-						auth_code := users.DoLogin(user_from.ID, for_root)
+						replayMsg = "See code in terminal with shell2telegram or ask code from root user and type:\n" + messageCmd + " code"
+						authCode := users.DoLogin(userFrom.ID, forRoot)
 
-						root_role_str := ""
-						if for_root {
-							root_role_str = "root "
+						rootRoleStr := ""
+						if forRoot {
+							rootRoleStr = "root "
 						}
-						secretCodeMsg := fmt.Sprintf("Request %saccess for %s. Code: %s\n", root_role_str, users.String(user_from.ID), auth_code)
+						secretCodeMsg := fmt.Sprintf("Request %saccess for %s. Code: %s\n", rootRoleStr, users.String(userFrom.ID), authCode)
 						fmt.Print(secretCodeMsg)
 						users.broadcastForRoots(bot, secretCodeMsg)
 
 					} else if len(messageText) > 1 {
-						if users.IsValidCode(user_from.ID, messageArgs, for_root) {
-							users.list[user_from.ID].IsAuthorized = true
-							if for_root {
-								users.list[user_from.ID].IsRoot = true
-								replay_msg = fmt.Sprintf("You (%s) authorized as root.", users.String(user_from.ID))
-								log.Print("root authorized: ", users.String(user_from.ID))
+						if users.IsValidCode(userFrom.ID, messageArgs, forRoot) {
+							users.list[userFrom.ID].IsAuthorized = true
+							if forRoot {
+								users.list[userFrom.ID].IsRoot = true
+								replayMsg = fmt.Sprintf("You (%s) authorized as root.", users.String(userFrom.ID))
+								log.Print("root authorized: ", users.String(userFrom.ID))
 							} else {
-								replay_msg = fmt.Sprintf("You (%s) authorized.", users.String(user_from.ID))
-								log.Print("authorized: ", users.String(user_from.ID))
+								replayMsg = fmt.Sprintf("You (%s) authorized.", users.String(userFrom.ID))
+								log.Print("authorized: ", users.String(userFrom.ID))
 							}
 						} else {
-							replay_msg = fmt.Sprintf("Code is not valid.")
+							replayMsg = fmt.Sprintf("Code is not valid.")
 						}
 					}
 
-				} else if messageCmd == "/help" {
+				case messageCmd == "/help":
 
 					if allowExec {
 						for cmd, shell_cmd := range commands {
-							replay_msg += fmt.Sprintf("%s - %s\n", cmd, shell_cmd)
+							replayMsg += fmt.Sprintf("%s - %s\n", cmd, shell_cmd)
 						}
-						if users.IsRoot(user_from.ID) {
-							replay_msg += fmt.Sprintf("%s - %s\n", "/shell2telegram stat", "get stat about users")
-							if app_config.addExit {
-								replay_msg += fmt.Sprintf("%s - %s\n", "/shell2telegram exit", "terminate bot")
+						if users.IsRoot(userFrom.ID) {
+							replayMsg += fmt.Sprintf("%s - %s\n", "/shell2telegram stat", "get stat about users")
+							if appConfig.addExit {
+								replayMsg += fmt.Sprintf("%s - %s\n", "/shell2telegram exit", "terminate bot")
 							}
 						}
 					}
-					replay_msg += fmt.Sprintf("%s - %s\n", "/auth [code]", "authorize user")
-					replay_msg += fmt.Sprintf("%s - %s\n", "/authroot [code]", "authorize user as root")
-					replay_msg += fmt.Sprintf("%s - %s\n", "/shell2telegram version", "show version")
+					replayMsg += fmt.Sprintf("%s - %s\n", "/auth [code]", "authorize user")
+					replayMsg += fmt.Sprintf("%s - %s\n", "/authroot [code]", "authorize user as root")
+					replayMsg += fmt.Sprintf("%s - %s\n", "/shell2telegram version", "show version")
 
-				} else if users.IsRoot(user_from.ID) && messageCmd == "/shell2telegram" && messageArgs == "stat" {
+				case messageCmd == "/shell2telegram" && messageArgs == "stat" && users.IsRoot(userFrom.ID):
 
-					for user_id, user := range users.list {
-						replay_msg += fmt.Sprintf("%s: auth: %v, root: %v, count: %d, last: %v\n",
-							users.String(user_id),
+					for userId, user := range users.list {
+						replayMsg += fmt.Sprintf("%s: auth: %v, root: %v, count: %d, last: %v\n",
+							users.String(userId),
 							user.IsAuthorized,
 							user.IsRoot,
 							user.Counter,
@@ -216,48 +216,50 @@ LOOP:
 						)
 					}
 
-				} else if users.IsRoot(user_from.ID) && app_config.addExit && messageCmd == "/shell2telegram" && messageArgs == "exit" {
+				case messageCmd == "/shell2telegram" && messageArgs == "exit" && users.IsRoot(userFrom.ID) && appConfig.addExit:
 
-					replay_msg = "bye..."
-					go_exit = true
+					replayMsg = "bye..."
+					doExit = true
 
-				} else if messageCmd == "/shell2telegram" && messageArgs == "version" {
+				case messageCmd == "/shell2telegram" && messageArgs == "version":
 
-					replay_msg = fmt.Sprintf("shell2telegram %s", VERSION)
+					replayMsg = fmt.Sprintf("shell2telegram %s", VERSION)
 
-				} else if cmd, found := commands[messageCmd]; allowExec && found {
+				case allowExec:
+					if cmd, found := commands[messageCmd]; found {
 
-					shell, params := "sh", []string{"-c", cmd}
-					os_exec_command := exec.Command(shell, params...)
-					os_exec_command.Stderr = os.Stderr
+						shell, params := "sh", []string{"-c", cmd}
+						osExecCommand := exec.Command(shell, params...)
+						osExecCommand.Stderr = os.Stderr
 
-					// write all arguments to STDIN
-					if messageArgs != "" {
-						stdin, err := os_exec_command.StdinPipe()
-						if err == nil {
-							io.WriteString(stdin, messageArgs)
-							stdin.Close()
+						// write all arguments to STDIN
+						if messageArgs != "" {
+							stdin, err := osExecCommand.StdinPipe()
+							if err == nil {
+								io.WriteString(stdin, messageArgs)
+								stdin.Close()
+							} else {
+								log.Print("get STDIN error: ", err)
+							}
+						}
+
+						shellOut, err := osExecCommand.Output()
+						if err != nil {
+							log.Print("exec error: ", err)
+							replayMsg = fmt.Sprintf("exec error: %s", err)
 						} else {
-							log.Print("get STDIN error: ", err)
+							replayMsg = string(shellOut)
 						}
 					}
+				} // switch for commands
 
-					shell_out, err := os_exec_command.Output()
-					if err != nil {
-						log.Print("exec error: ", err)
-						replay_msg = fmt.Sprintf("exec error: %s", err)
-					} else {
-						replay_msg = string(shell_out)
-					}
-				}
-
-				if replay_msg != "" {
-					sendMessageWithLogging(bot, chat_id, replay_msg)
-					if app_config.logCommands {
-						log.Printf("%d @%s: %s", user_from.ID, user_from.UserName, telegram_update.Message.Text)
+				if replayMsg != "" {
+					sendMessageWithLogging(bot, telegramUpdate.Message.Chat.ID, replayMsg)
+					if appConfig.logCommands {
+						log.Printf("%d @%s: %s", userFrom.ID, userFrom.UserName, telegramUpdate.Message.Text)
 					}
 
-					if go_exit {
+					if doExit {
 						break LOOP
 					}
 				}
